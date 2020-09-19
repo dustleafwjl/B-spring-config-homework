01e2bf351310f25a732c833b48e2b34477341616
dustleafwjl <wangjianglin_priv@qq.com>
2020-09-19 23:20:48 +0800

    [jianglin] feat: finished p1s1

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
@@ -2,12 +2,18 @@ package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+import java.util.Map;
+
 @ConfigurationProperties("mail")
 public class MailConfig {
 
     private String hostname;
     private int port;
     private String from;
+    private List defaultRecipients;
+    private Map additionalHeaders;
+    private Credentials credentials;
 
     public String getHostname() {
         return hostname;
@@ -33,12 +39,79 @@ public class MailConfig {
         this.from = from;
     }
 
+    public List getDefaultRecipients() {
+        return defaultRecipients;
+    }
+
+    public void setDefaultRecipients(List defaultRecipients) {
+        this.defaultRecipients = defaultRecipients;
+    }
+
+    public Map getAdditionalHeaders() {
+        return additionalHeaders;
+    }
+
+    public void setAdditionalHeaders(Map additionalHeaders) {
+        this.additionalHeaders = additionalHeaders;
+    }
+
+    public Credentials getCredentials() {
+        return credentials;
+    }
+
+    public void setCredentials(Credentials credentials) {
+        this.credentials = credentials;
+    }
+
     @Override
     public String toString() {
         return "MailConfig{" +
                 "hostname='" + hostname + '\'' +
                 ", port=" + port +
                 ", from='" + from + '\'' +
+                ", defaultRecipients=" + defaultRecipients +
+                ", additionalHeaders=" + additionalHeaders +
+                ", credentials=" + credentials +
                 '}';
+    }
+
+    public static class Credentials {
+        private String username;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getAuthMethod() {
+            return authMethod;
+        }
+
+        public void setAuthMethod(String authMethod) {
+            this.authMethod = authMethod;
+        }
+
+        private String password;
+        private String authMethod;
+
+        @Override
+        public String toString() {
+            return "Credentials{" +
+                    "username='" + username + '\'' +
+                    ", password='" + password + '\'' +
+                    ", authMethod='" + authMethod + '\'' +
+                    '}';
+        }
     }
 }
