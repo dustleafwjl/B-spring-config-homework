674ad14676e6e9c65be2a96c6acf6fad94990e33
dustleafwjl <wangjianglin_priv@qq.com>
2020-09-19 23:30:09 +0800

    [jianglin] feat: p2s3

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -1,13 +1,24 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest(properties = "levelNumber=3")
+@AutoConfigureMockMvc
 class DemoSpringConfigApplicationTests {
 
+	@Autowired
+	MockMvc mockMvc;
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		mockMvc.perform(get("/level")).andExpect(jsonPath("$", is("advanced")));
 	}
 
 }
